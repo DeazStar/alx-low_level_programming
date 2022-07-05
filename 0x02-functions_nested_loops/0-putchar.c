9c27a625dfac050c2d8ed6a71de92854c8e1f586
DeazStar <deadfromtheborn@gmail.com>
2022-07-05 06:48:47 -0400

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,6 +2,7 @@
 
 /**
  * main - prints _putchar, fllowd by a new linees
+ * 
  * Return: Alwys 0 (Success)
  */
 int main(void)
@@ -14,6 +15,7 @@ int main(void)
 	{
 		_putchar(abe[c]);
 	}
+
 	_putchar('\n');
 	return (0);
 }
