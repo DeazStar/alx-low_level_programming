17c2389c899baed11055b791234bc71f13718ca2
DeazStar <deadfromtheborn@gmail.com>
2022-10-21 17:02:41 +0300

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    hash_node_t *tmp;
+    hash_node_t *next = NULL;
+    unsigned int i;
+    if (ht == NULL)
+        return;
+
+
+    for (i = 0; i < ht->size; i++)
+    {
+        if (ht->array[i] == NULL)
+        {
+            continue;
+        }
+        else
+        {
+            tmp = ht->array[i];
+
+            while(tmp != NULL)
+            {
+                next = tmp;
+                tmp = tmp->next;
+                free(next->key);
+                free(next->value);
+                free(next);
+                next = NULL;
+            }
+            ht->array[i] = NULL;
+        }
+    }
+
+    free(ht->array);
+    free(tmp);
+    free(ht);
+}
