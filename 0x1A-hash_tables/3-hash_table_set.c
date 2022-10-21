ecb608bcab269f190952264b2338b6181f30aca5
DeazStar <deadfromtheborn@gmail.com>
2022-10-21 17:23:19 +0300

    fix for leakage of memeory

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,50 +1,53 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element to the hash table
- * @ht: The pointer to the hash table
- * @key: The key to add
- * @value: The value associated with key
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
  *
- * Return: if Success - 1 otherwise - 0
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
-	hash_node_t *head_node;
-	unsigned long int idx, i;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	new = malloc(sizeof(hash_node_t));
-
-	if (new == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
 
-	idx = key_index((const unsigned char *)key, ht->size);
-
-	for (i = idx; ht->array[i]; i++)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = strdup(value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
 
-	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = NULL;
-
-	if (ht->array[idx] != NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		head_node = ht->array[idx];
-		new->next = head_node;
+		free(value_copy);
+		return (0);
 	}
-
-	ht->array[idx] = new;
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
