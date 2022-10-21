580005c45342052b6cb4a4bf0d31633194f598ed
DeazStar <deadfromtheborn@gmail.com>
2022-10-21 17:04:58 +0300

    fix-betty-style

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,37 +6,37 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *tmp;
-    hash_node_t *next = NULL;
-    unsigned int i;
-    if (ht == NULL)
-        return;
+	hash_node_t *tmp;
+	hash_node_t *next = NULL;
+	unsigned int i;
+	if (ht == NULL)
+		return;
 
 
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] == NULL)
-        {
-            continue;
-        }
-        else
-        {
-            tmp = ht->array[i];
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+		{
+			continue;
+		}
+		else
+		{
+			tmp = ht->array[i];
 
-            while(tmp != NULL)
-            {
-                next = tmp;
-                tmp = tmp->next;
-                free(next->key);
-                free(next->value);
-                free(next);
-                next = NULL;
-            }
-            ht->array[i] = NULL;
-        }
-    }
+			while(tmp != NULL)
+			{
+				next = tmp;
+				tmp = tmp->next;
+				free(next->key);
+				free(next->value);
+				free(next);
+				next = NULL;
+			}
+			ht->array[i] = NULL;
+		}
+	}
 
-    free(ht->array);
-    free(tmp);
-    free(ht);
+	free(ht->array);
+	free(tmp);
+	free(ht);
 }
