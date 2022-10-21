538c569b58ada4f2aabfa2622565304cea32f19a
DeazStar <deadfromtheborn@gmail.com>
2022-10-21 12:12:21 +0300

    add 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create:  a function that creates a hash table
+ * @size: the size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hash_tb;
+
+    unsigned long int i;
+
+    hash_tb = malloc(sizeof(hash_table_t));
+
+    if (hash_tb == NULL)
+    {
+        return NULL;
+    }
+
+    hash_tb->size = size;
+    hash_tb->array = malloc(sizeof(hash_node_t) * size);
+
+    if (hash_tb->array == NULL)
+    {
+        return NULL;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        hash_tb[i] = NULL;
+    }
+
+    return (hash_tb);
+
+}
