6d02eaa5808395e46dfee0e4ce80a373e774a89a
DeazStar <deadfromtheborn@gmail.com>
2022-10-21 12:25:19 +0300

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,18 +10,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_tb;
-
 	unsigned long int i;
 
 	hash_tb = malloc(sizeof(hash_table_t));
-
 	if (hash_tb == NULL)
 	{
 		return (NULL);
 	}
 
 	hash_tb->size = size;
-	hash_tb->array = malloc(sizeof(hash_node_t) * size);
+	hash_tb->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash_tb->array == NULL)
 	{
